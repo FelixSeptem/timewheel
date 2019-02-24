1affe5135964cb765366cba709c03e2a1073b26d
FelixSeptem <whenhow94@gmail.com>
2019-02-24 22:18:55 +0800

    fix ci

--- example/example.go
+++ example/example.go
@@ -27,21 +27,36 @@ func main() {
 		fmt.Printf("running after 15 second with %s\n", time.Now().Format("2006-01-02 15:04:05.999"))
 		return nil
 	}
-	tw.Run()
-	tw.AddTask(time.Second, fun1s)
-	tw.AddTask(time.Second*7, fun7s)
-	tw.AddTask(time.Second*17, fun7s)
-	tw.AddTask(time.Second*10, fun10s)
-	tw.AddTask(time.Second*20, fun10s)
-	tw.AddTask(time.Second*30, fun10s)
-	tw.AddTask(time.Second*15, fun15s)
+	if err := tw.Run(); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second, fun1s); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second*7, fun7s); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second*17, fun7s); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second*10, fun10s); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second*20, fun10s); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second*30, fun10s); err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := tw.AddTask(time.Second*15, fun15s); err != nil {
+		fmt.Println(err.Error())
+	}
 	time.Sleep(time.Second * 60)
 }
 
 func handleErr(tw *timewheel.TimeWheel) {
 	errs := tw.HandleErr()
-	select {
-	case err := <-errs:
-		fmt.Println(err)
+	for {
+		fmt.Println(<-errs)
 	}
 }
